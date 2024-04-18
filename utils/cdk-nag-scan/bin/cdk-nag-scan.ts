ddf927420b5477f3209fc4572119d188a07887b2
Nate Ferrell <nateferl@amazon.com>
2024-04-18 09:56:56 -0500

    feat: #comment clarity added to stack name generation code

--- utils/cdk-nag-scan/bin/cdk-nag-scan.ts
+++ utils/cdk-nag-scan/bin/cdk-nag-scan.ts
@@ -11,6 +11,17 @@ if (!templateFileName) {
   throw new Error('fileName is required');
 }
 
+/*
+ * This uses the input file name to generate the temp stack name.
+ * The temp stack name uses up to 128 characters of the path-name
+ * (CloudFormation stack name length limit) to the CloudFormation
+ * template that is found, slugifying non-whitespace characters to
+ * scalar hyphens (-).
+ *
+ * This is done to ensure that the output files are unique and do
+ * not overwrite each other when a scanned repository has multiple
+ * CloudFormation template found to scan.
+ */
 var stackName = templateFileName
   .replace(/\/(src|run|out|work)\//, '')
   .replace(/[\W_]+/gi, '-')
