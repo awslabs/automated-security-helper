16bde36c586f1f350ec06f8d9200e6521896b504
Nate Ferrell <nateferl@amazon.com>
2025-06-10 22:49:50 -0500

    feat(ci): added additional steps to plugin evaluation to handle more edge cases, updated CI tests as well

--- automated_security_helper/plugin_modules/ash_builtin/scanners/opengrep_scanner.py
+++ automated_security_helper/plugin_modules/ash_builtin/scanners/opengrep_scanner.py
@@ -97,7 +97,7 @@ class OpengrepScannerConfigOptions(ScannerOptionsBase):
 class OpengrepScannerConfig(ScannerPluginConfigBase):
     name: Literal["opengrep"] = "opengrep"
     # Enabled by default on Windows as Semgrep does not support Windows directly
-    enabled: bool = platform.system().lower() == "windows"
+    enabled: bool = platform.system().lower().startswith("win")
     options: Annotated[
         OpengrepScannerConfigOptions, Field(description="Configure Opengrep scanner")
     ] = OpengrepScannerConfigOptions()
